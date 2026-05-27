ad87d8315f5d6efbc7cac8f1ad38072306b08af6
Victor <victor_hugo_moreira@hotmail.com>
2026-05-26 21:35:06 -0300

    first commit

---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -0,0 +1,20 @@
+namespace ClubeDaLeitura.ConsoleApp.Compartilhado;
+
+public class TelaPrincipal()
+{
+    public string ObterMenuPrincipal()
+    {
+
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Clube da Leitura");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("1 - Gerenciar caixas de revistas");
+        Console.WriteLine("2 - Gerenciar revistas");
+        Console.WriteLine("3 - Gerenciar amigos");
+        Console.WriteLine("4 - Gerenciar empréstimos");
+        Console.WriteLine("S - Sair");
+        Console.WriteLine("---------------------------------");
+        return;
+    }
+}
